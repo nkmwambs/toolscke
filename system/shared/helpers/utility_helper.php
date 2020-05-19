27922213525275583e44c59d654019f7f394e0a6
nkmwambs <nkmwambs@gmail.com>
2020-05-19 11:40:37 +0300

    Commented the shared utility healper

--- system/shared/helpers/utility_helper.php
+++ system/shared/helpers/utility_helper.php
@@ -1,9 +1,10 @@
-    <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+    <?php 
+    // if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-     if ( ! function_exists('asset_url'))
-     {
-       function asset_url()
-       {
-          return base_url().'../assets/';
-       }
-     }
+    //  if ( ! function_exists('asset_url'))
+    //  {
+    //    function asset_url()
+    //    {
+    //       return base_url().'../assets/';
+    //    }
+    //  }
