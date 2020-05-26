2a565a7a58318e022b4f3f1d74a004df6ac474d8
nkmwambs <nkmwambs@gmail.com>
2020-05-26 11:31:15 +0300

    Removed admin.php from gitignore

---
+++ claims/controllers/Admin.php
@@ -0,0 +1,50 @@
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/*	
+ *	@author 	: Joyonto Roy
+ *	date		: 27 september, 2014
+ *	FPS School Management System Pro
+ *	http://codecanyon.net/user/FreePhpSoftwares
+ *	support@freephpsoftwares.com
+ */
+
+class Admin extends CI_Controller
+{
+    
+    
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+        $this->load->library('session');
+		
+       /*cache control*/
+		$this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+		$this->output->set_header('Pragma: no-cache');
+		
+    }
+    
+    /***default functin, redirects to login page if no admin logged in yet***/
+    public function index()
+    {
+        if ($this->session->userdata('admin_login') != 1)
+            redirect(base_url() . 'login', 'refresh');
+    }
+    
+    /***ADMIN DASHBOARD***/
+    function dashboard()
+    {
+        if ($this->session->userdata('admin_login') != 1)
+            redirect(base_url().'admin.php','refresh');
+			
+        $page_data['page_name']  = 'dashboard';
+        $page_data['page_title'] = get_phrase('claims_dashboard');
+        $this->load->view('backend/index', $page_data);
+    }
+	
+
+  
+    
+}
