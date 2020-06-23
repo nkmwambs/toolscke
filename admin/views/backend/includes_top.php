6d1782c7f28be2036630f5f2760420842256d37a
root <root@server.compassionkenya.com>
2020-06-23 15:51:53 +0300

    Correcting PHP errors revealed by logs in admin app

--- admin/views/backend/includes_top.php
+++ admin/views/backend/includes_top.php
@@ -1,9 +1,16 @@
+
+<!-- Bootstrap CDN -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	
+<!--Jquery CDN Minified -->
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>
+
 <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
 
 <link rel="stylesheet" href="<?php echo base_url();?>assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
 <link rel="stylesheet" href="<?php echo base_url();?>assets/css/font-icons/entypo/css/entypo.css">
 <!--<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">-->
-<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css">
+<!--<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css">-->
 <link rel="stylesheet" href="<?php echo base_url();?>assets/css/neon-core.css">
 <link rel="stylesheet" href="<?php echo base_url();?>assets/css/neon-theme.css">
 <link rel="stylesheet" href="<?php echo base_url();?>assets/css/neon-forms.css">
@@ -23,7 +30,7 @@
 <?php if ($text_align == 'right-to-left') : ?>
     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/neon-rtl.css">
 <?php endif; ?>
-<script src="<?php echo base_url();?>assets/js/jquery-1.11.0.min.js"></script>
+<!--<script src="<?php echo base_url();?>assets/js/jquery-1.11.0.min.js"></script>-->
 
         <!--[if lt IE 9]><script src="assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
 
